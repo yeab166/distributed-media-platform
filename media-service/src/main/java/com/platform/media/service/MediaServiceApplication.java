77bb43523d8a1dfa48a3a40e1c33b6546b5fcc62
yidenek5 <yeabsira.ayele@aastustudent.edu.et>
2026-05-30 22:52:47 +0300

    Add Spring Boot entry point for media service

---
+++ media-service/src/main/java/com/platform/media/service/MediaServiceApplication.java
@@ -0,0 +1,11 @@
+package com.platform.media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MediaServiceApplication.class, args);
+    }
+}
